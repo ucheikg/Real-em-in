5876579cc0261fc29cd2928b1ee991bc8d28205a
HARRY CLAPP <HC312513@falmouth.ac.uk>
2025-01-28 17:17:30 +0000

    Player controls with new input system working loop menu and lederboard

---
+++ Assets/Script/Leaderboard.cs
@@ -0,0 +1,45 @@
+using Dan.Main;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+
+    private void Start()
+    {
+        UpdateLeaderboard();
+    }
+
+    #region Leaderboard
+
+    [Header("Leaderboard")]
+    [SerializeField] private List<TextMeshProUGUI> names;
+    [SerializeField] private List<TextMeshProUGUI> scores;
+
+    private string publicLeaderboardKey =
+        "4b700994758b3fff08b281e618b7d5fb2692440741b690d1805d86bbde879684";
+
+    public void UpdateLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) => {
+            int loopLength = (msg.Length < names.Count) ? msg.Length : names.Count;
+            for (int i = 0; i < loopLength; i++)
+            {
+                names[i].text = msg[i].Username;
+                scores[i].text = msg[i].Score.ToString();
+            }
+        }));
+    }
+
+    public void SetLeaderboardEntry(string username, int score)
+    {
+        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username,
+            score, ((msg) => {
+                UpdateLeaderboard();
+            }));
+    }
+
+    #endregion
+}
---
+++ Assets/Script/StartButton.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class StartButton : MonoBehaviour
+{
+
+    private Devices Device = Devices.Keyboard;
+    private TextMeshProUGUI tmp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmp = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(Device)
+        {
+            case Devices.Keyboard:
+                tmp.text = "press enter to continue...";
+                break;
+            case Devices.Gamepad:
+                tmp.text = "press button to continue...";
+                break;
+        }
+    }
+
+    private void OnEnterGame()
+    {
+        Debug.Log("Entering");
+        StartCoroutine(loadNameSelectScene());
+    }
+
+    IEnumerator loadNameSelectScene()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("NameSelect");
+    }
+
+
+    void CheckDevice()
+    {
+        if (Input.anyKey)
+        {
+            Device = Devices.Keyboard;
+        }
+
+        if (Input.GetJoystickNames().Length > 0)
+        {
+            foreach (string joystickName in Input.GetJoystickNames())
+            {
+                if (!string.IsNullOrEmpty(joystickName))
+                {
+                    Device = Devices.Gamepad;
+                }
+            }
+        }
+    }
+
+    public enum Devices
+    {
+        Keyboard,
+        Gamepad
+    }
+}
