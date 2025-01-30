cee65aa3d57718a7025f23f67747664a46afeeb6
HARRY CLAPP <HC312513@falmouth.ac.uk>
2025-01-30 12:15:05 +0000

    Added Inventory script to a non destroyable object to stay in scene.

--- Assets/Script/GameSettings.cs
+++ Assets/Script/GameSettings.cs
@@ -7,18 +7,18 @@ using Dan.Models;
 
 public class GameSettings : MonoBehaviour
 {
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
 
     private void Start()
     {
-        UpdateLeaderboard();
 
         if(PlayerPrefs.GetString("PlayerName") != null)
         {
             SetPlayerName(PlayerPrefs.GetString("PlayerName"));
-        }
-        else
-        {
-
         }
 
         string print = string.Empty;
@@ -55,37 +55,6 @@ public class GameSettings : MonoBehaviour
     }
 
 
-    #region Leaderboard
-
-    [Header("Leaderboard")]
-    [SerializeField] private List<TextMeshProUGUI> names;
-    [SerializeField] private List<TextMeshProUGUI> scores;
-
-    private string publicLeaderboardKey =
-        "a94df36f87cbc1abbda1642d9a3f48b943bdb7cba8ae7a7786cac83984dfed02";
-    
-    public void UpdateLeaderboard()
-    {
-        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) => {
-            int loopLength = (msg.Length < names.Count) ? msg.Length : names.Count;
-            for(int i = 0; i < loopLength; i++)
-            {
-                names[i].text = msg[i].Username;
-                scores[i].text = msg[i].Score.ToString();
-            }
-        }));
-    }
-
-    public void SetLeaderboardEntry(string username, int score)
-    {
-        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, 
-            score, ((msg) => {
-                UpdateLeaderboard();
-            }));
-    }
-
-    #endregion
-    [Space(5)]
 
     #region Player
     [Header("Player")]
