e08a89ee0b4851de0c9482d8ba8dfa3ab93889e5
UCHEOMA EMELONYE <UE315957@falmouth.ac.uk>
2025-04-29 16:25:54 +0100

    joystick is now functioning gonna add to unity

--- Arduino Code/Draft2.0/sketch_apr08a/sketch_apr08a.ino
+++ Arduino Code/Draft2.0/sketch_apr08a/sketch_apr08a.ino
@@ -1,16 +1,33 @@
+
+const int SW_pin = 2; //digital pin connected to switch output
+const int X_pin = A0; //analog pin connected to x output
+const int Y_pin = A1; //analog pin connected to y output
+bool up;
+bool down;
+bool right;
+bool left;
+int deadzone = 300;
+
+
 const int clkPin = 5;
 const int dtPin = 4;
 const int swPin = 3;
+
 char handshake = 'x';
+
 int threshold = 90;
 int counter = 0;
 int counterMin = 0;
 int counterMax = 100;
+
 bool spinning = false;
+
 int lastClkState;
 unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 5;
 unsigned long lastDecrementTime = 0;
+
+
 void setup() {
   Serial.begin(9600);
   pinMode(clkPin, INPUT_PULLUP);
@@ -18,7 +35,7 @@ void setup() {
   pinMode(swPin, INPUT_PULLUP);
   lastClkState = digitalRead(clkPin);
 
-   bool online = false;
+  bool online = false;
   while (!online) {
     while (!Serial.available()) {
     }
@@ -52,10 +69,47 @@ void loop() {
       spinning = false;
     }
   }
-  if (counter > threshold){
+  if (counter > threshold) {
     spinning = true;
-  } else if (counter < threshold){
+  } else if (counter < threshold) {
     spinning = false;
+  }
+
+
+  int x = analogRead(X_pin);
+  int y = analogRead(Y_pin);
+
+  if (533 + deadzone < x && x <= 1023) {
+    right = true;
+    left = false;
+  }
+
+
+  if (0 <= x && x < 0 + deadzone) {
+    right = false;
+    left = true;
+  }
+
+  if (533 - deadzone <= x && x <= 533 + deadzone) {
+    right = false;
+    left = false;
+  }
+
+
+  if (533 + deadzone < y && y <= 1023) {
+    up = true;
+    down = false;
+  }
+
+
+  if (0 <= y && y < 0 + deadzone) {
+    up = false;
+    down = true;
+  }
+
+  if (533 - deadzone <= y && y <= 533 + deadzone) {
+    up = false;
+    down = false;
   }
 
   if (!Serial.available()) {
